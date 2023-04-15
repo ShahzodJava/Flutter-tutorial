a69f6e688f13c7738d7863409f897cb72891cb03
Shahzod-Ashurmatov <75681220+ShahzodJava@users.noreply.github.com>
2023-04-15 18:13:24 +0500

    World Time App

---
+++ WorldTime/lib/pages/loading.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:world_time/services/world_time.dart';
+
+class Loading extends StatefulWidget {
+  const Loading({Key? key}) : super(key: key);
+
+  @override
+  State<Loading> createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+
+  void setupWorldTime() async {
+    WorldTime instance = WorldTime(
+        location: 'Tashkent', flag: 'germany.png', url: 'Asia/Tashkent');
+    await instance.getTime();
+    Navigator.pushReplacementNamed(context, '/home',arguments: {
+      'location': instance.location,
+      'flag': instance.flag,
+      'time': instance.time,
+      'isDaytime': instance.isDaytime ?? true
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupWorldTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.blue,
+      body: Center(
+        child:  SpinKitCircle(
+          color: Colors.white,
+          size: 50.0,
+        ),
+      ),
+    );
+  }
+}
